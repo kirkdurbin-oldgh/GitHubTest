186fdc89e47957e62da8ed62e1c4bac2b9b4388c
Kirk <Kirk@Kirk-PC>
2012-05-15 15:10:19 -0400

    first commit

---
+++ src/githubtest/GitHubTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author Kirk
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("This is a GitHub test.");
+        // TODO code application logic here
+    }
+}
